e39d952ad6caf4c8083acb3fe4ba6c5fdfb3406a
azha <anatole.zha@gmail.com>
2018-12-05 16:11:38 +0100

    Add data.json and the display of it

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const dotenv = require('dotenv').config();
+const data = require('./data');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({}));
+app.get('/', (req, res) => res.json(data));
 
 app.listen(process.env.PORT, () => console.log('Listening...'));
