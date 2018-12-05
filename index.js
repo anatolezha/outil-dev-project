c39a17108de4e57e5d6fe81e35384a1bc11d296d
azha <anatole.zha@gmail.com>
2018-12-05 16:57:04 +0100

    Add put request route

--- index.js
+++ index.js
@@ -46,5 +46,55 @@ app.delete('/:title', (req, res) => {
   res.status(404).end();
 });
 
-app.listen(process.env.PORT, () => console.log('Listening...'));
+app.put('/:title', bodyParser.json(), (req, res) => {
+  const title = req.params.title;
+  const index = data.findIndex(d => d.title === title);
+  if (index >= 0) {
+    const title = req.body.title;
+    if (title && typeof title !== 'string') {
+      return res.status(400).end();
+    }
+    const type = req.body.type;
+    if (type && (typeof type !== 'number' || (type <= 0 || type > 4))) {
+      return res.status(400).end();
+    }
+    const details = req.body.details;
+    if (details && typeof details !== 'string') {
+      return res.status(400).end();
+    }
+    const coordinates = req.body.coordinates;
+    if (coordinates && typeof coordinates !== 'object') {
+      return res.status(400).end();
+    }
+    if (coordinates && coordinates.lat && typeof coordinates.lat !== 'number') {
+      return res.status(400).end();
+    }
+    if (coordinates && coordinates.lon && typeof coordinates.lon !== 'number') {
+      return res.status(400).end();
+    }
+    if (title) {
+      data[index].title = title;
+    }
+    if (details) {
+      data[index].details = details;
+    }
+    if (type) {
+      data[index].type = type;
+    }
+    if (coordinates) {
+      const c = data[index].coordinates || {};
+      if (coordinates.lat) {
+        c.lat = coordinates.lat;
+      }
+      if (coordinates.lon) {
+        c.lon = coordinates.lon;
+      }
+      data[index].coordinates = c;
+    }
+    return res.status(204).end();
+  }
+  res.status(404).end();
 
+});
+
+app.listen(process.env.PORT, () => console.log('Listening...'));
